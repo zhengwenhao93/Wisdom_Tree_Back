8b8e42b4e428d265a0daf418ab2a228d28099fdf
zhengwenhao <931516643@qq.com>
2022-07-11 16:44:24 +0800

    feat(eslin+跨域): 需要的配置选项

---
+++ src/views/login/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="">
+    login页面
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}, // 注册组件
+  methods: {}, // 方法
+  computed: {} // 计算
+}
+</script>
+<style lang="scss" scoped></style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
+module.exports = ({
+  transpileDependencies: true,
+  lintOnSave: false,
+  devServer: {
+    open: true,
+    https: false,
+    port: 9999,
+    host: 'localhost',
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: process.env.VUE_APP_SERVICE_URL,
+        changeOrigin: true,
+        PathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: ''
+        }
+      }
+    }
+  }
 })
